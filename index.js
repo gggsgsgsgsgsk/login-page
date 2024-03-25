52c0ab77812a328a22ac7c50c7b8a1562dd86d4a
gggsgsgsgsgsk <145146140+gggsgsgsgsgsk@users.noreply.github.com>
2024-03-25 15:06:25 +0000

    Update index.js
    change redirection to rmmv4 html file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function login() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  
-  if (username === 'admin' && password === 'codydorkrulez') {
-    window.location.href = 'https://rblxmodmenuv4.wordpress.com';
-  } else {
-    alert('Invalid username or password. Please try again.');
-  }
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === 'codydorkrulez') {
+        window.location.href = 'rblxmodmenuv4.wordpress.html';
+    } else {
+        alert('Invalid credentials. Please try again.');
+    }
 }
